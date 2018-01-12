180933ef2f090405b755393aebb1869cce98ced5
rachit-ranjan16 <rachit.ranjan93@gmail.com>
2018-01-11 19:49:51 -0500

    Check Balanced Symbols Using Stacks Create Stack with passed Input Sequence Associated Test Cases

--- dat_struct_py/tests/testStacks.py
+++ dat_struct_py/tests/testStacks.py
@@ -6,6 +6,13 @@ class testStacks(unittest.TestCase):
     def setUp(self):
         self.stack = lStack()
 
+    def testStackCreationWithInputListAndLimit(self):
+        inp_list = [x+1 for x in range(5)]
+        another_stack = lStack(limit=6,inp_list=inp_list)
+        self.assertEqual(another_stack.isFull(), False)
+        self.assertEqual(another_stack.isEmpty(), False)
+        self.assertEqual(another_stack.peek(), 5)
+
     def testPushAndPeek(self):
         self.assertEqual(self.stack.isEmpty(), True)
         for i in range(5):
@@ -31,3 +38,10 @@ class testStacks(unittest.TestCase):
         self.assertEqual(self.stack.pop(), 1)
         self.assertIsNone(self.stack.pop())
         self.assertIsNone(self.stack.peek())
+
+    def testBalancedWithStack(self):
+        sym_stack = lStack(25)
+        self.assertEqual(sym_stack.symbols_balanced('[]'), True)
+        self.assertEqual(sym_stack.symbols_balanced('[({{()}})]'), True)
+        self.assertEqual(sym_stack.symbols_balanced('[{)]]]]}]'), False)
+        self.assertEqual(sym_stack.symbols_balanced('[({{()}}]'), False)
